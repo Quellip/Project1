8b270e0cddf8969ead6312cdd3360cd52e373199
жбанков никита <498171@mail.ru>
2024-11-05 23:14:49 +0300

    masks upgrade

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
-def get_mask_card_number(card_number: str) -> str:
+def get_mask_card_number(card: str) -> str:
     """Функция, которая возвращает зашифрованный вид карты пользователя """
-    private_number = card_number[:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
+    private_number = card[:6] + (len(card[6:-4]) * "*") + card[-4:]
     chunks, chunk_size = len(private_number), len(private_number) // 4
     mask_card = " ".join([private_number[i:i + chunk_size] for i in range(0, chunks, chunk_size)])
     return mask_card
