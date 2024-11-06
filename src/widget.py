fdd744a84b2d4ba71ac852c06609ce2d5a4cf319
жбанков никита <498171@mail.ru>
2024-11-06 19:42:31 +0300

    upgrade widget

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,16 @@
+import src.masks
+
 def mask_account_card(card_name: str) -> str:
     """Функция, возвращающая зашифрованный номер карты"""
     card_number = card_name.split()[-1]
-    private_number = card_number[:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
-    chunks, chunk_size = len(private_number), len(private_number) // 4
-    mask_card = " ".join([private_number[i : i + chunk_size] for i in range(0, chunks, chunk_size)])
+    if card_name.split()[0] == 'Счет':
+        mask_card = src.masks.get_mask_account(card_number)
+    else:
+        mask_card = src.masks.get_mask_card_number(card_number)
+
     return mask_card
 
 
 def get_date(date: str) -> str:
-    """Функция, возвращающая дату операции"""
+    """Функция, возвращающая дату операции """
     return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
