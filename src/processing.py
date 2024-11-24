23c4cf78bc2a288601456f6d85e6b17638a4abbd
Quell <Ty3eMeLL@gmail.com>
2024-11-24 18:22:34 +0300

    finish homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
-def filter_by_state(list_dicts: list, state="EXECUTED") -> list:
+def filter_by_state(list_dicts: list, state: str = "EXECUTED") -> list:
     """Функция, принимающая список словарей и возвращающая новый список"""
     new_list_dicts = []
-    for words in list_dicts:
-        for key, value in words.items():
-            if value == state:
-                new_list_dicts.append(words)
+    for dicts in list_dicts:
+        if dicts["state"] == state:
+            new_list_dicts.append(dicts)
 
     return new_list_dicts
 
 
-def sort_by_date(list_dicts: list, reverse=True) -> list:
+def sort_by_date(list_dicts: list, switch: bool = True) -> list:
     """Функция принимающая список словарей и сортирующая по сроку давности"""
-    return sorted(list_dicts, key=lambda d: d["date"], reverse=True)
+    return sorted(list_dicts, key=lambda d: d["date"], reverse=switch)
