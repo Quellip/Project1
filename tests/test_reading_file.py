3dcd02be4081f7a05b3d8ffdf3a58b2f9b29a803
Quell <Ty3eMeLL@gmail.com>
2025-01-11 21:27:57 +0300

    final version

--- tests/test_reading_file.py
+++ tests/test_reading_file.py
@@ -1,11 +1,12 @@
 from unittest.mock import mock_open, patch
+from typing import Any
 
 import pandas as pd
 
 from src.reading_file import get_reading_csv, get_reading_exel
 
 
-def test_get_reading_csv():
+def test_get_reading_csv() -> None:
     mock_data = (
         "id;state;date;amount\n650703;EXECUTED;"
         "2023-09-05T11:30:32Z;16210\n3598919;EXECUTED;2020-12-06T23:00:58Z;29740"
@@ -19,13 +20,13 @@ def test_get_reading_csv():
     assert result == expected
 
 
-def test_get_reading_csv_not_file():
+def test_get_reading_csv_not_file() -> None:
     result = get_reading_csv("test_transactions1.csv")
     assert result == []
 
 
 @patch("src.reading_file.pd.read_excel")
-def test_get_reading_exel(mock_get):
+def test_get_reading_exel(mock_get: Any) -> None:
     expected_data = "test_exel.xlsx"
     mock_get.return_value = pd.DataFrame(
         [{"id": 650703, "state": "EXECUTED", "date": "2023-09-05T11:30:32Z", "amount": 16210}]
@@ -35,8 +36,8 @@ def test_get_reading_exel(mock_get):
 
 
 @patch("src.reading_file.pd.read_excel")
-def test_get_reading_exel_empty(mock_get):
+def test_get_reading_exel_empty(mock_get: Any) -> None:
     expected_data = "test_exel_empty.xlsx"
     mock_get.return_value = pd.DataFrame([])
-    expected_result = []
+    expected_result: list[Any] = []
     assert get_reading_exel(expected_data) == expected_result
